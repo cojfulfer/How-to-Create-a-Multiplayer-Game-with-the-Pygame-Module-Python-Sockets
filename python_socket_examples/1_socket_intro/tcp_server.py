a71827f5c97880a00dcd630230e838f27c8f2a73
cojfulfer <conjfulfer@gmail.com>
2023-01-30 16:53:55 -0600

    had the TCP client socket decode and print the message sent from the TCP server socket and closed both sockets by following along with https://www.youtube.com/watch?v=Vi-IyRp_I_o&list=WL&index=6&t=1706s from 00:51:47 to 00:58:11

--- python_socket_examples/1_socket_intro/tcp_server.py
+++ python_socket_examples/1_socket_intro/tcp_server.py
@@ -5,9 +5,9 @@ import socket
 # create a server side socket using IPv4 (AF_INET) & TCP (SOCK_STREAM)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# see how to get IP address dynamically
-print(socket.gethostname()) # hostname
-print(socket.gethostbyname(socket.gethostname())) # IP address of the given hostname
+# # see how to get IP address dynamically
+# print(socket.gethostname()) # hostname
+# print(socket.gethostbyname(socket.gethostname())) # IP address of the given hostname
 
 # bind our new socket to a tuple (IP address, port address)
 server_socket.bind((socket.gethostbyname(socket.gethostname()), 12345))
@@ -19,12 +19,16 @@ server_socket.listen()
 while True:
     # accept every single connection & store 2 pieces of info
     client_socket, client_address = server_socket.accept() # returns a tuple of the client socket obj & a tuple of the client IPv4 address & port address that the client is using for the connection (not the same port address that client is connecting to)
-    print(type(client_socket))
-    print(client_socket)
-    print(type(client_address))
-    print(client_address)
+    # print(type(client_socket))
+    # print(client_socket)
+    # print(type(client_address))
+    # print(client_address)
 
     print(f"Connected to {client_address}!\n")
 
     # send a message to the client that just connected
     client_socket.send("You are connected!".encode("utf-8"))
+
+    # close the connection
+    server_socket.close()
+    break
