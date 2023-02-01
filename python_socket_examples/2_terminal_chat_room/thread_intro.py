0b5640245f21338fe08f1fc7aa1c8949294fc33c
cojfulfer <conjfulfer@gmail.com>
2023-01-31 23:30:10 -0600

    following along with https://www.youtube.com/watch?v=Vi-IyRp_I_o&list=WL&index=6&t=1706s from 01:45:41 to 01:53:24: ¶ called the 'start()' function on 3 'Thread()' class objects to run the 3 functions concurrently ¶ redefined one of the 'Thread()' class objects to rerun it ¶ paused the main program until the thread is complete by calling the 'join()' function on the 'Thread()' class object

--- python_socket_examples/2_terminal_chat_room/thread_intro.py
+++ python_socket_examples/2_terminal_chat_room/thread_intro.py
@@ -22,6 +22,25 @@ def function3():
 
 # if we call these functions, we see the 1st function call MUST complete before the next
 # they are executed linearly
-function1()
-function2()
-function3()
+# function1()
+# function2()
+# function3()
+
+# WE can execute these functions concurrently using threads! We must have a target for a thread.
+t1 = threading.Thread(target=function1)
+t2 = threading.Thread(target=function2)
+t3 = threading.Thread(target=function3)
+
+# t1.start()
+# t2.start()
+# t3.start()
+
+# Threads can only be run once. If you want to reuse, you must redefine
+t1 = threading.Thread(target=function1)
+t1.start()
+
+# if you want to 'pause' the main program until a thread is done you can!
+t1 = threading.Thread(target=function1)
+t1.start()
+t1.join() # this pauses the main program until the thread is complete
+print("Threading rules!")
