462898c19690b65108ae73256e3dabea8a7379c1
cojfulfer <conjfulfer@gmail.com>
2023-02-02 22:36:45 -0600

    added frames (input and output frames) to the root window along with adding widgets to those frames (message entry and send button) by following along with https://www.youtube.com/watch?v=Vi-IyRp_I_o&list=WL&index=6&t=1706s from 2:54:23 to 3:08:43

--- python_socket_examples/3_basic_gui_chat_room/tkinter_intro.py
+++ python_socket_examples/3_basic_gui_chat_room/tkinter_intro.py
@@ -1,5 +1,6 @@
 # Tkinter introduction
 import tkinter
+from tkinter import BOTH
 
 # define window
 root = tkinter.Tk()
@@ -8,7 +9,7 @@ root.iconbitmap('blue_talk.png')
 root.geometry('400x600')
 root.resizable(0,0)
 
-# define colors
+# define colors (in hexadecimal)
 root_color = "#535657"
 input_color = "#4f646f"
 output_color = "#dee7e7"
@@ -18,9 +19,17 @@ root.config(bg=root_color)
 
 
 # define GUI layout
+# define frames (that are placed on the root window)
+input_frame = tkinter.LabelFrame(root, bg=input_color)
+output_frame = tkinter.LabelFrame(root, bg=output_color)
+input_frame.pack(pady=10)
+output_frame.pack(padx=10, pady=(0,10), fill=BOTH, expand=True)
 
-
-# run the root window's mainloop()
+# define widgets (that are placed on the frames)
+message_entry = tkinter.Entry(input_frame, text="Enter a message", width=30)
+send_button = tkinter.Button(input_frame, text="Send")
+message_entry.grid(row=0, column=0, padx=10, pady=10)
+send_button.grid(row=0, column=1, padx=10, pady=10) # did not include "ipadx=20, ipady=5" as the button gets cut off
 
 # run the root window's mainloop()
 root.mainloop()
