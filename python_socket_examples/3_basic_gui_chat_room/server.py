1c702f6b21571755e853380652c8f9a3a8010e12
cojfulfer <conjfulfer@gmail.com>
2023-02-05 00:32:33 -0600

    set up a basic GUI chatroom on the client side by following along with https://www.youtube.com/watch?v=Vi-IyRp_I_o&list=WL&index=6&t=1706s from 3:31:23 to 3:52:16: ¶ copy & pasted the 'server.py' script from the '2_terminal_chat_room' directory as a template for the '3_basic_gui_chat_room' directory ¶ created info, output, & input frames to go onto the root window within the 'client.py' script of the '3_basic_gui_chat_room' directory ¶ placed label (name & ip), entry (name & ip), & button (connect & disconnect) widgets onto the info frame within the 'client.py' script of the '3_basic_gui_chat_room' directory

---
+++ python_socket_examples/3_basic_gui_chat_room/server.py
@@ -0,0 +1,92 @@
+# Server Side Chat Room
+import socket, threading
+
+# define constants to be used
+HOST_IP = socket.gethostbyname(socket.gethostname())
+HOST_PORT = 12345
+ENCODER = "utf-8"
+BYTE_SIZE = 1024
+
+# create a server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST_IP, HOST_PORT))
+server_socket.listen()
+
+# create two blank lists to store connected client sockets & their names
+client_socket_list = []
+client_name_list = []
+
+
+def broadcast_message(message):
+    '''Send a message to ALL clients connected to the server'''
+    # I feel as if there could be a socket function that broadcasts a message so we wouldn't need to define & call our own function
+    for client_socket in client_socket_list:
+        client_socket.send(message)
+
+
+def receive_message(client_socket, client_address): # I added 'client_address'
+    '''Receive an incoming message from a specific client & forward the message to be broadcast'''
+    while True:
+        try:
+            # get the name of the given client
+            index = client_socket_list.index(client_socket)
+            name = client_name_list[index]
+
+            # receive message from the client
+            message = client_socket.recv(BYTE_SIZE).decode(ENCODER)
+            message = f"\033[1;92m\t{name}: {message}\033[0m".encode(ENCODER) # bright green, bolded message tabbed in
+            broadcast_message(message)
+        except:
+            # find the index of the client socket in our list
+            # I don't understand why we have to redefine 'index' & 'name', seems redundant
+            index = client_socket_list.index(client_socket)
+            name = client_name_list[index]
+
+            # remove the client socket & name from lists
+            client_socket_list.remove(client_socket)
+            client_name_list.remove(name)
+
+            # close the client socket
+            client_socket.close()
+
+            # broadcast that the client has left the chat
+            broadcast_message(f"\033[5;91m\t{name} has left the chat!\033[0m".encode(ENCODER)) # bright red, blinking message tabbed in
+            print(f"{client_address} disconnected") # I added this
+            break
+
+
+def connect_client():
+    '''Connect an incoming client to the server'''
+    while True:
+        # accept any incoming client connection
+        client_socket, client_address = server_socket.accept()
+        print(f"Connected with {client_address}...")
+
+        # send a NAME flag to prompt the client for their name
+        client_socket.send("NAME".encode(ENCODER))
+        client_name = client_socket.recv(BYTE_SIZE).decode(ENCODER)
+
+        # add new client socket & client name to appropriate lists
+        client_socket_list.append(client_socket)
+        client_name_list.append(client_name)
+
+        # update the server, individual client, & ALL clients
+        print(f"Name of new client is {client_name}\n") # server
+        client_socket.send(f"{client_name}, you have connected to the server!".encode(ENCODER)) # individual client
+        broadcast_message(f"\n{client_name} has joined the chat!".encode(ENCODER)) # all clients # I added the '\n'
+
+        # now that a new client has connected, start a thread
+        receive_thread = threading.Thread(target=receive_message, args=(client_socket, client_address,)) # I added 'client_address'
+        receive_thread.start()
+
+
+# start the server
+print("Server is listening for incoming connections...")
+connect_client()
+
+# I don't understand why the below code wouldn't be more efficient...
+# connect_thread = threading.thread(target=connect_client)
+# connect_thread.start()
+
+
+
