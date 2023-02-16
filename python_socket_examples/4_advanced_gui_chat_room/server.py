fb16ec37e6563697dc648be068a620a9c76d0d71
cojfulfer <conjfulfer@gmail.com>
2023-02-15 23:55:04 -0600

    continued to build the server layout along with outlining the server functionality for the advanced GUI chat room by following along with https://www.youtube.com/watch?v=Vi-IyRp_I_o&list=WL&index=6&t=1706s from 5:51:50 to 6:11:50: ¶ defined and placed scrollbar and listbox widgets onto the client frame ¶ defined and placed message input entry and self broadcast button widgets onto the message frame ¶ defined and placed message, kick, and ban button widgets onto the admin frame ¶ outlined 'Connection()' class to store a connection which includes a server socket and pertinent information ¶ outlined 11 server functions: 'start_server()', 'end_server()', 'connect_client()', 'create_message()', 'process_message()', 'broadcast_message()', 'receive_message()','self_broadcast()', 'private_message()', 'kick_client()', and 'ban_client()'

--- python_socket_examples/4_advanced_gui_chat_room/server.py
+++ python_socket_examples/4_advanced_gui_chat_room/server.py
@@ -18,8 +18,67 @@ light_green = "#1fc742"
 root.config(bg=black)
 
 
-# define functions
+# create a connection class to hold our server socket
+class Connection():
+    '''A class to store a connection - a server socket & pertinent information'''
+    def __init___(self):
+        pass
 
+
+# define functions
+def start_server(connection)
+    '''Start the server on a given port number'''
+    pass
+
+
+def end_server(connection)
+    '''Begin the process of ending the server'''
+    pass
+
+
+def connect_client(connection):
+    '''Connect an incoming client to the server'''
+    pass
+
+
+def create_message(flag, name, message, color)
+    '''Return a message packet to be sent'''
+    pass
+
+
+def process_message(connection, message_json, client_socket, client_address=(0,0))
+    '''Update server information based on a message packet flag'''
+    pass
+
+
+def broadcast_message(connection, message_json):
+    "Send a message to all client sockets connected to the server"
+    pass
+
+
+def receive_message(connection, client_socket):
+    "Receive an incoming message from a client"
+    pass
+
+
+def self_broadcast(connection):
+    "Broadcast a special admin message to all clients"
+    pass
+
+
+def private_message(connection):
+    "Send a private message to a single client"
+    pass
+
+
+def kick_client(connection):
+    "kick a given client off the server"
+    pass
+
+
+def ban_client(connection):
+    "Ban a given client based on their IP address"
+    pass
 
 
 # define GUI layout
@@ -56,8 +115,30 @@ history_scrollbar.config(command=history_listbox.yview)
 history_listbox.grid(row=0, column=0)
 history_scrollbar.grid(row=0, column=1, sticky="NS") # 'sticky="NS"' attribute expands scroll bar vertically north & south
 
+# client frame layout
+client_scrollbar = tkinter.Scrollbar(client_frame, orient=VERTICAL)
+client_listbox = tkinter.Listbox(client_frame, height=10, width=55, borderwidth=3, font=my_font, bg=black, fg=light_green, yscrollcommand=client_scrollbar.set) # my listbox outline doesn't display like in the tutorial
+# to synchronize the scroll bar & listbox so that the scroll bar changes the vertical view of the listbox
+client_scrollbar.config(command=client_listbox.yview)
 
+client_listbox.grid(row=0, column=0)
+client_scrollbar.grid(row=0, column=1, sticky="NS") # 'sticky="NS"' attribute expands scroll bar vertically north & south
 
+# message frame layout
+input_entry = tkinter.Entry(message_frame, width=40, borderwidth=3, font=my_font)
+self_broadcast_button = tkinter.Button(message_frame, text="Broadcast", width=13, borderwidth=5, font=my_font, bg=light_green, state=DISABLED)
+
+input_entry.grid(row=0, column=0, padx=5, pady=5)
+self_broadcast_button.grid(row=0, column=1, padx=5, pady=5)
+
+# admin frame layout
+message_button = tkinter.Button(admin_frame, text="PM", borderwidth=5, width=15, font=my_font, bg=light_green, state=DISABLED)
+kick_button = tkinter.Button(admin_frame, text="Kick", borderwidth=5, width=15, font=my_font, bg=light_green, state=DISABLED)
+ban_button = tkinter.Button(admin_frame, text="Ban", borderwidth=5, width=15, font=my_font, bg=light_green, state=DISABLED)
+
+message_button.grid(row=0, column=0, padx=5, pady=5)
+kick_button.grid(row=0, column=1, padx=5, pady=5)
+ban_button.grid(row=0, column=2, padx=5, pady=5)
 
 
 
